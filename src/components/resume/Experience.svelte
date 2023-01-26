6f857b92bb2a8748fa2d13c658bd0090d7c5583f
Jackson Salopek <jackson.salopek@gmail.com>
2023-01-25 23:07:50 -0500

    Updates to resume

--- src/components/resume/Experience.svelte
+++ src/components/resume/Experience.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
-	import UnderConstruction from '../UnderConstruction.svelte'
 	import ResumeSectionHeader from './ResumeSectionHeader.svelte'
 	import { onMount } from 'svelte'
 	import { EVENTS, track } from 'src/shared/analytics'
 	import type { ResumeSection } from 'src/shared/resume'
 
 	export let section: ResumeSection<'work'>
+	export let experiences = section.work
+	export const getMonthString = (month?: number) => {
+		if (!month) return '01'
+		if (month < 10) return `0${month}`
+		else return `${month}`
+	}
 
 	onMount(() => {
 		track(EVENTS.load.component.resume.experience)
@@ -14,5 +19,33 @@
 
 <div class="js-resume-experience">
 	<ResumeSectionHeader>Experience</ResumeSectionHeader>
-	<UnderConstruction />
+	{#each experiences as experience}
+		<div class="js-resume-experience-item">
+			<span class="js-resume-experience-item-header">
+				{#if experience.url !== ''}
+					<a href={experience.url}
+						><h4 class="js-resume-experience-company">{experience.company}</h4></a
+					>
+				{:else}
+					<h4 class="js-resume-experience-company">{experience.company}</h4>
+				{/if}
+				<h5 class="js-resume-experience-duration">
+					{#if experience.isCurrentRole}
+						{experience.start.year}.{getMonthString(experience.start.month)} - PRESENT
+					{:else}
+						{experience.start.year}.{getMonthString(experience.start.month)} - {experience.end
+							.year}.{getMonthString(experience.end.month)}
+					{/if}
+				</h5>
+			</span>
+			<div class="js-resume-experience-highlights">
+				<ul style="list-style-type: '› '">
+					{#each experience.highlights as highlight}
+						<li><span class="js-resume-experience-highlight">{highlight}</span></li>
+					{/each}
+				</ul>
+			</div>
+		</div>
+		<br />
+	{/each}
 </div>
