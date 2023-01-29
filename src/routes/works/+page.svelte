729e9a411c4d58d7d28920be1cb13d8eddbdae4a
Jackson Salopek <jackson.salopek@gmail.com>
2023-01-29 14:19:27 -0500

    Links

--- src/routes/works/+page.svelte
+++ src/routes/works/+page.svelte
@@ -7,3 +7,11 @@
 	<meta name="description" content="Jackson Salopek's Works" />
 </svelte:head>
 <UnderConstruction />
+
+<!-- https://teoti.io/ -->
+<!-- https://terrapay.money/ -->
+<!-- https://old.reddit.com/r/chelseafc/ -->
+<!-- https://old.reddit.com/r/LudwigAhgren/ -->
+<!-- https://old.reddit.com/r/PointCrow/ -->
+<!-- https://www.youtube.com/watch?v=iuppiXxyzjY -->
+<!-- https://github.com/jacksonsalopek/temporal -->
